0e670598595da622c1e0d89b4109bfcf53fe382f
TechCrent <ciicicrentsil@gmail.com>
2026-06-12 22:39:13 +0000

    feat(monolith): scaffold Spring Boot monolith with Flyway baseline (v0.1-005)

--- monolith/src/main/java/com/stash/MonolithApplication.java
+++ monolith/src/main/java/com/stash/MonolithApplication.java
@@ -3,7 +3,10 @@ package com.stash;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
 public class MonolithApplication {
     public static void main(String[] args) {
         SpringApplication.run(MonolithApplication.class, args);

ddaed8c661fbbd1f52f469c27f2647e036a8122d
TechCrent <ciicicrentsil@gmail.com>
2026-06-13 11:56:11 +0000

    feat(kyc): scaffold KYC Service with Flyway baseline (v0.1-007)

--- kyc-service/src/main/java/com/stash/kyc/KycServiceApplication.java
+++ kyc-service/src/main/java/com/stash/kyc/KycServiceApplication.java
@@ -3,8 +3,12 @@ package com.stash.kyc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
 public class KycServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(KycServiceApplication.class, args);
     }
